f5d3ba107f57853ad790d96d6cafa286a44a9db5
UnknownExplorer13 <kyedylanfenton@gmail.com>
2023-06-18 14:45:17 +0930

    Add BSXFlags automation spell implemented. Templates don't work right now as I can't get the QComboBox currentIndexChanged signal to work properly (commented out code related to them).

--- src/spells/automate.cpp
+++ src/spells/automate.cpp
@@ -74,7 +74,114 @@ public:
 
 		return cmb;
 	}
+
 };
+
+// Automatically setup NiStringExtraData (and BSBehaviorGraphExtraData if needed) block(s) for weapons
+class spAutoAddBSXFlags final : public Spell, uiAutomateDialogSetup
+{
+public:
+	QString name() const override final { return Spell::tr( "Add BSXFlags" ); }
+	QString page() const override final { return Spell::tr( "Automate" ); }
+
+	bool isApplicable( const NifModel * nif, const QModelIndex & index ) override final
+	{
+		return nif && ( ( nif->getBlockName( index ) == "BSFadeNode" ) && ( nif->getBlockNumber( index ) == 0 ) ) && ( nif->checkVersion( 0x14020007, 0x14020007 ) && nif->getUserVersion2() == 83 || 100 );
+	}
+
+	QModelIndex cast( NifModel * nif, const QModelIndex & index ) override final
+	{
+		QModelIndex iBlock = nif->getBlock( index );
+
+		if ( iBlock.isValid() ) {
+			quint32 flags = nif->get<int>( index );
+
+			QDialog dlg;
+			QVBoxLayout * vbox = new QVBoxLayout( &dlg );
+			//QStringList templateChoices = { "None", "Animated Static", "Clutter", "Static" };
+			//QComboBox * cmbOptions = dlgCombo( vbox, Spell::tr( "Template" ), templateChoices );  //Templates don't work yet, commenting out for now
+			//vbox->addWidget( new QLabel( "Templates" ) );
+			//vbox->addWidget( new QLabel( "" ) );
+			vbox->addWidget( new QLabel( "Flags to enable:" ) );
+
+			QStringList flagChoices {
+									  "Has Animations",                                 //Animated (1)
+									  "Enable Collision",                               //Havok (2)
+									  "Is a \"skeleton.nif\" file",                     //Ragdoll (4)
+									  "Multiple Collision Blocks",                      //Complex (8)
+									  "Uses Addon Nodes (e.g. candle flames)",          //Addon (16)
+									  "Has Editor Marker(s)",                           //Editor Marker (32)
+									  "Has Havok Physics (e.g. helmet ground model)",   //Dynamic (64)
+									  "Has One Collision Block and/or Animation",       //Articulated (128)
+									  "Has Moving Parts",                               //Needs Transform Updates (256)
+									  "Enable External Emittance in CK"                 //External Emit (512)
+									};
+
+			/* // Add commented out flag values as seen in flags.cpp; Disabled by default
+			QStringList flagChoicesExtra {
+										   "Magic Shader Properties",            //1024
+										   "Lights",                             //2048
+										   "Breakable",                          //4096
+										   "Searched Breakable (Runtime Only?)"  //8192
+										 };
+			flagChoices.append(flagChoicesExtra); */
+
+			QList<QCheckBox *> chkBoxes;
+			int x = 0;
+			for ( const QString& flagName : flagChoices ) {
+				chkBoxes << dlgCheck( vbox, QString( "%1" ).arg( flagName ) );
+				x++;
+			}
+			chkBoxes.at( 8 )->hide(); //Hides the Needs Transform Updates option as Skyrim doesn't use it
+
+			dlgButtons( &dlg, vbox, "Add BSXFlags" );
+
+			if ( dlg.exec() == QDialog::Accepted ) {
+				QModelIndex extraData;
+				QModelIndex iArray = nif->getIndex( iBlock, "Extra Data List" );
+				bool iBSXFExists = false;
+
+				// Check if a BSXFlags block already exists, if it does then fill extraData value
+				for ( int i = 0; i < nif->getBlockCount(); i++ ) {
+					QModelIndex iBlockCheck = nif->getBlock( i );
+
+					if ( nif->getBlockName( iBlockCheck ) == "BSXFlags" ) {
+						iBSXFExists = true;
+						extraData = nif->getBlock( i );
+						break;
+					}
+				}
+
+				// BSXFlags block does not exist
+				// Add the new BSXFlags block and fill extraData value with the new block
+				if ( iBSXFExists == false ) {
+					auto newData = nif->insertNiBlock( "BSXFlags" );
+					extraData = newData;
+					nif->set<uint>( iBlock, "Num Extra Data List", ( nif->get<int>(iBlock, "Num Extra Data List") + 1 ) );
+					nif->updateArray( iArray );
+					nif->setLink( iArray.child( nif->get<int>(iBlock, "Num Extra Data List") - 1, 0 ), nif->getBlockNumber( extraData ) );
+				}
+
+				nif->set<QString>( extraData, "Name", "BSX" );
+
+				// Get chkBoxes int value and toggle BSXFlags values accordingly
+				x = 0;
+				for ( QCheckBox * chk : chkBoxes ) {
+					flags = ( flags & ( ~( 1 << x ) ) ) | ( chk->isChecked() ? 1 << x : 0 );
+					x++;
+				}
+				nif->set<int>( extraData, "Integer Data", flags );
+				
+			}
+
+		}
+
+		return QModelIndex();
+	}
+
+};
+
+REGISTER_SPELL( spAutoAddBSXFlags );
 
 // Automatically setup NiStringExtraData (and BSBehaviorGraphExtraData if needed) block(s) for weapons
 class spAutoAddWeaponData final : public Spell, uiAutomateDialogSetup
@@ -98,7 +205,7 @@ public:
 
 			QStringList weaponChoices = { "Arrow/Bolt", "Battleaxe/Greatsword/Warhammer", "Bow", "Crossbow", "Dagger", "Mace", "Shield", "Sword", "War Axe" };
 
-			QComboBox * cmbOptions = dlgCombo( vbox, Spell::tr( "What type of weapon is this?" ), weaponChoices );
+			QComboBox * cmbOptions = dlgCombo( vbox, Spell::tr( "Weapon type:" ), weaponChoices );
 
 			dlgButtons( &dlg, vbox, "Add Data" );
 
